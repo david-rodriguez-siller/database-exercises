ee32ea405950ae7580e5e30f222defe779c44bee
“David <David.rodriguez.siller@gmail.com>
2021-09-21 09:24:25 -0500

    LIMIT SQL exercies

--- limit_exercises.sql
+++ limit_exercises.sql
@@ -1,6 +1,6 @@
 -- 1:
 USE employees;
--- 2:
+-- 2: Zykh, Zyda, Zwicker, Zweizig, Zumaque, Zultner, Zucker, Zuberek, Zschoche, Zongker
 SELECT DISTINCT last_name FROM employees ORDER BY last_name DESC LIMIT 10;
 -- 3: The five employees are Alsel Cappello, Utz Mandell, Bouchung Schreiter, Boacai Kushner, and Petter Stroustrup
 SELECT first_name, last_name, hire_date, birth_date FROM employees WHERE hire_date LIKE '199%' AND birth_date LIKE '%12-25' ORDER BY hire_date LIMIT 5;
